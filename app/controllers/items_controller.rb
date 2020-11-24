67182dad6879e486ac0ccced40f8e0065d1cb7df
Martin Nordlund <nordlund.martin@gmail.com>
2020-11-24 18:31:59 +0100

    added list of other items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,19 @@
 class ItemsController < ApplicationController
-  
+before_action :set_item, only: :show
   def index
     @items = policy_scope(Item).order(created_at: :desc)
   end
 
   def show
-    authorize @item
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:id])
+    @items = Item.all
+    authorize @item
+
   end
 
 end

4bc620e3803d0a9050039e7ec388ddd270e76e06
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-29 20:15:14 +0100

    messages, chatroom, pundit, policies added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,15 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
+    authorize @message
     if @message.save
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show"
     end
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end

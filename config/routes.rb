fa7bb9affb0e7ff51e57b47114fd07983aa015c9
Martin Nordlund <nordlund.martin@gmail.com>
2020-12-01 14:28:59 +0100

    added drop up to users icon in navbar, and added inbox page

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,9 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :chatrooms, only: :show
 
 
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [ :index, :show ] do
     resources :messages, only: :create
   end
 end

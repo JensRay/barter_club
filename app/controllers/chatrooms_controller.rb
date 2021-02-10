a1785337cccd9ba36bbe84e5f21915e5dc5358b9
Martin Nordlund <nordlund.martin@gmail.com>
2021-02-10 21:04:02 +0100

    notification works

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,11 +4,9 @@ class ChatroomsController < ApplicationController
 
   def index
     @chatrooms = policy_scope(Chatroom).order(created_at: :desc)
-
-
   end
+  
   def show
-
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
 
